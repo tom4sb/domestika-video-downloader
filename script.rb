0260ab958b5560de16d24c9ccf92a4d748ff6946
adulces <amtomas@proton.me>
2024-11-04 20:22:11 +0100

    Script to save videos with the info from a CSV file

---
+++ script.rb
@@ -0,0 +1,46 @@
+# Libraries
+require "csv"
+
+# Command templates
+FFMPEG_COMMAND_TEMPLATE = "ffmpeg -i \"%{m3u8_file_url}\" -c copy \"./output/%{course_title}/%{mp4_file_name}.mp4\""
+MKDIR_COMMAND_TEMPLATE = "mkdir \"./output/%{course_title}\""
+
+# Classes
+class VideoInfo
+    attr_reader :number, :title, :m3u8_url
+
+    def initialize(number, title, m3u8_url)
+        @number = number
+        @title = title
+        @m3u8_url = m3u8_url
+    end
+end
+
+# Methods
+def capitalize_words(string)
+    string.split(' ').map(&:capitalize).join(' ')
+end
+
+##############
+### Script ###
+##############
+
+# Load CSV
+course_csv_file = File.new(Dir["./input/*"].first.to_s, "r")
+video_infos = Array.new
+CSV.foreach(course_csv_file, headers: false) do |row|
+    video_infos << VideoInfo.new(row[0], row[1], row[2])
+end
+
+# Create directory
+course_title = capitalize_words(File.basename(course_csv_file, ".csv"))
+mkdir_command = MKDIR_COMMAND_TEMPLATE % {course_title: course_title}
+%x[#{mkdir_command}]
+
+# Save videos
+video_infos.each do |video_info|
+    m3u8_file_url = video_info.m3u8_url
+    mp4_file_name = "[#{video_info.number.rjust(2, "0")}] #{capitalize_words(video_info.title)}"
+    ffmpeg_command = FFMPEG_COMMAND_TEMPLATE % {m3u8_file_url: m3u8_file_url, course_title: course_title, mp4_file_name: mp4_file_name}
+    %x[#{ffmpeg_command}]
+end
