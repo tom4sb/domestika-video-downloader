7e1ff6abe933e5835b6047cc6daa7acb208fc799
adulces <187294208+devtbr@users.noreply.github.com>
2024-11-13 22:28:21 +0100

    Refactor to separate logic in methods

--- script.rb
+++ script.rb
@@ -1,9 +1,12 @@
 # Libraries
 require "csv"
 
+# Constants
+OUTPUT_PATH = "./output"
+
 # String templates
-FFMPEG_COMMAND_TEMPLATE = "ffmpeg -i \"%{m3u8_file_url}\" -c copy \"./output/%{course_title}/%{mp4_file_name}.mp4\""
-MKDIR_COMMAND_TEMPLATE = "mkdir -p \"./output/%{course_title}\""
+FFMPEG_COMMAND_TEMPLATE = "ffmpeg -i \"%{m3u8_file_url}\" -c copy \"%{course_output_path}/%{mp4_file_name}.mp4\""
+MKDIR_COMMAND_TEMPLATE = "mkdir -p \"%{course_output_path}\""
 MP4_FILE_NAME_TEMPLATE = "[%{video_number}] %{video_title}"
 
 # Classes
@@ -18,6 +21,45 @@ class VideoInfo
 end
 
 # Methods
+def process_file(file)
+    course_csv_file = File.new(file.to_s, "r")
+    course_output_path = build_course_output_path(course_csv_file)
+    video_infos = load_video_infos(course_csv_file)
+    create_course_output_directory(course_output_path)
+    save_videos(video_infos, course_output_path)
+end
+
+def build_course_output_path(course_csv_file)
+    course_title = capitalize_words(File.basename(course_csv_file, ".csv"))
+    "#{OUTPUT_PATH}/#{course_title}"
+end
+
+def load_video_infos(course_csv_file)
+    video_infos = Array.new
+    CSV.foreach(course_csv_file, headers: false) { |row| video_infos << VideoInfo.new(*row) }
+    video_infos
+end
+
+def create_course_output_directory(course_output_path)
+    mkdir_command = MKDIR_COMMAND_TEMPLATE % { course_output_path: course_output_path }
+    %x[#{mkdir_command}]
+end
+
+def save_videos(video_infos, course_output_path)
+    video_infos.each do |video_info|
+        mp4_file_name = MP4_FILE_NAME_TEMPLATE % {
+            video_number: video_info.number.rjust(2, "0"),
+            video_title: capitalize_words(video_info.title)
+        }
+        ffmpeg_command = FFMPEG_COMMAND_TEMPLATE % {
+            m3u8_file_url: video_info.m3u8_url,
+            course_output_path: course_output_path,
+            mp4_file_name: mp4_file_name
+        }
+        %x[#{ffmpeg_command}]
+    end
+end
+
 def capitalize_words(string)
     string.split(' ').map(&:capitalize).join(' ')
 end
@@ -26,22 +68,4 @@ end
 ### Script ###
 ##############
 
-Dir["./input/*"].each do |file|
-    # Load CSV
-    course_csv_file = File.new(file.to_s, "r")
-    video_infos = Array.new
-    CSV.foreach(course_csv_file, headers: false) { |row| video_infos << VideoInfo.new(*row) }
-
-    # Create directory
-    course_title = capitalize_words(File.basename(course_csv_file, ".csv"))
-    mkdir_command = MKDIR_COMMAND_TEMPLATE % { course_title: course_title }
-    %x[#{mkdir_command}]
-
-    # Save videos
-    video_infos.each do |video_info|
-        m3u8_file_url = video_info.m3u8_url
-        mp4_file_name = MP4_FILE_NAME_TEMPLATE % { video_number: video_info.number.rjust(2, "0"), video_title: capitalize_words(video_info.title) }
-        ffmpeg_command = FFMPEG_COMMAND_TEMPLATE % { m3u8_file_url: m3u8_file_url, course_title: course_title, mp4_file_name: mp4_file_name }
-        %x[#{ffmpeg_command}]
-    end
-end
+Dir["./input/*"].each(&method(:process_file))
